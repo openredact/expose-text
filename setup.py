e727a99efaacb56946ebe9957cbd0454146c60cf
malteos <7977470+malteos@users.noreply.github.com>
2020-05-15 09:08:01 +0200

    Added include_package_data

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name="expose-text",
     version="0.1.0a",
     packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
     license="MIT",
     description="A Python module that exposes text for modification in multiple file types.",
     long_description=open("README.md").read(),
